5e0abca48443b1da32345104cd1f435c0764f622
jventura1738 <justinv9001@aol.com>
2021-10-23 05:10:50 -0400

    Fix grid_traveler name

--- grids/grid_traveler.py
+++ grids/grid_traveler.py
@@ -3,15 +3,21 @@
 """
 Count how many possible paths there are from the top left
 corner to the bottom right corner.
+
+(0, x) -> 0
+(x, 0) -> 0
+(1, 1) -> 1
+(2, 3) -> 3
 """
 
 import sys
+from typing import Dict
 
 
-def grid_travler(rows: int, cols: int) -> int:
+def grid_traveler(rows: int, cols: int, memo: Dict) -> int:
     pass
 
 
 if __name__ == '__main__':
     assert(len(sys.argv) == 3)
-    grid_travler(sys.argv[1:])
+    grid_traveler(sys.argv[1:])
