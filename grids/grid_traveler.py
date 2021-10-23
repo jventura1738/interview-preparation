e9014bcd74ee22cce76d5030775076a052c519a7
jventura1738 <justinv9001@aol.com>
2021-10-22 20:07:39 -0400

    Add grid traveler problem

---
+++ grids/grid_traveler.py
@@ -0,0 +1,17 @@
+# Justin Ventura
+
+"""
+Count how many possible paths there are from the top left
+corner to the bottom right corner.
+"""
+
+import sys
+
+
+def grid_travler(rows: int, cols: int) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    assert(len(sys.argv) == 3)
+    grid_travler(sys.argv[1:])
