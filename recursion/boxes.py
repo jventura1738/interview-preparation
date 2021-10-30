a8a54da574f0ec14fdcdd9bdf770a028a0ae9d72
jventura1738 <justinv9001@aol.com>
2021-10-30 16:51:28 -0400

    Add recursion problem that I choked during an interview

---
+++ recursion/boxes.py
@@ -0,0 +1,24 @@
+# Justin Ventura
+
+def num_of_piles(n, m, p):
+    return split(n//p, m, p, n%p)
+
+
+def split(n, m, p, extra):
+    piles = [n]*p
+    for i in range(len(piles)):
+        if extra > 0:
+            piles[i] += 1
+            extra -= 1
+    total = 0
+    for pile in piles:
+        if pile <= m:
+            total += 1 if pile != 0 else 0
+        else:
+            total += split(pile//p, m, p, pile%p)
+    return total
+
+
+if __name__ == '__main__':
+    assert num_of_piles(11, 2, 2) == 7
+    print(num_of_piles(11, 2, 2))
