6bbdedf66f865d23cb6de40bfa2a512b90717444
jventura1738 <justinv9001@aol.com>
2022-03-16 20:08:14 -0400

    Refactor solution: more readable

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,24 +1,24 @@
 # Justin Ventura Amazon Prep
 
-import sys
 from typing import List
 
 
-def two_sum(self, nums: List[int], target: int) -> List[int]:
+def two_sum(nums: List[int], target: int) -> List[int]:
     """Find the unique solution i, j where a[i] + a[j] = target
 
     Complexity( n=len(nums) ):
         Time: O(n + dict_lookup)
         Space: O(n)
     """
-    d = {}
-    for i, e in enumerate(nums):
-        tmp = target - e
-        if tmp in d.keys():
-            return [d[tmp], i]
-        d[e] = i
+    seen = dict()
+    for i, num in enumerate(nums):
+        if target - num in seen:
+            return [seen[target - num], i]
+        seen[num] = i
     return []
 
 
 if __name__ == "__main__":
-    print(two_sum([2, 7, 11, 15], 9))
+    result = two_sum([2, 7, 11, 15], 9)
+    assert result == [0, 1]
+    print(result)
