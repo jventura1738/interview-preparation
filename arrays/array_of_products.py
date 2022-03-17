f0b777d79225f539730fc4acb5f3ae3e6d7ac467
jventura1738 <justinv9001@aol.com>
2022-03-17 11:21:52 -0400

    Fix format

--- arrays/array_of_products.py
+++ arrays/array_of_products.py
@@ -5,8 +5,8 @@ def arrayOfProducts(arr):
     left_prod = [1]*len(arr)
     current_product = 1
     for i in range(len(arr)):
-	    left_prod[i] = current_product
-	    current_product *= arr[i]
+        left_prod[i] = current_product
+        current_product *= arr[i]
     current_product = 1
     for i in range(len(arr)-1, -1, -1):
         left_prod[i] *= current_product
@@ -15,7 +15,7 @@ def arrayOfProducts(arr):
 
 
 if __name__ == '__main__':
-    test=[5, 1, 4, 2]
-    result=arrayOfProducts(test)
+    test = [5, 1, 4, 2]
+    result = arrayOfProducts(test)
     print(result)
     assert result == [8, 40, 10, 20]
