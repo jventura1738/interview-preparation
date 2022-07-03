623e911b97a16121c1b49a76890ab0f0f0a15020
Justin Ventura <justinv9001@aol.com>
2022-07-02 22:22:11 -0700

    Add sol for lc easy Google interview question

---
+++ stacks/stack_based_queue.py
@@ -0,0 +1,43 @@
+# Justin Ventura
+
+class MyQueue:
+
+    def __init__(self):
+        self.instack, self.outstack = list(), list()
+
+    def push(self, x: int) -> None:
+        self.instack.append(x)
+
+    def pop(self) -> int:
+        # Edge case:
+        if len(self.instack) == 0:
+            return None
+
+        return self.reshape('pop')
+
+    def peek(self) -> int:
+        # Edge case:
+        if len(self.instack) == 0:
+            return None
+
+        return self.reshape('peek')
+
+    def reshape(self, op: str) -> int:
+        # Shape outstack:
+        while self.instack:
+            self.outstack.append(self.instack.pop())
+
+        # Pop off the end:
+        if op == 'peek':
+            value = self.outstack[-1]
+        elif op == 'pop':
+            value = self.outstack.pop()
+
+        # Shape instack:
+        while self.outstack:
+            self.instack.append(self.outstack.pop())
+
+        return value
+
+    def empty(self) -> bool:
+        return len(self.instack) == 0
